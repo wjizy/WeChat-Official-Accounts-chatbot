05fcdfc24e5ad6a1a0247b588f3adb510c0b8e7a
wjizy <742491161@qq.com>
2017-02-20 21:47:19 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 class chat
 {
-	public function valid()
-	{
+	public function valid(){
   		$signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];	
-		$token = 'lingpai';//令牌
+        	$timestamp = $_GET["timestamp"];
+        	$nonce = $_GET["nonce"];	
+		$token = 'lingpai';//你的令牌
 		$tmpArr = array($token, $timestamp, $nonce);
 		sort($tmpArr, SORT_STRING);
 		$tmpStr = implode( $tmpArr );
@@ -19,13 +18,11 @@ class chat
 		}
 	}
 
-	public function logmess()
-	{
+	public function logmess(){
 		file_put_contents('log.xml',$GLOBALS['HTTP_RAW_POST_DATA']);
 	}
 
-	public function reponse()
-	{
+	public function reponse(){
 		//xml字符串转换为xml对象
 		if(empty($GLOBALS['HTTP_RAW_POST_DATA'])){
 			echo '咋啥也不说';
@@ -36,46 +33,36 @@ class chat
 		file_put_contents('receivelog.xml',$result);
 		echo $result;//发给微信服务器
 	}
-	
+
 	//返回消息
-	public function receiveText($obj)
-	{
+	public function receiveText($obj){
 		$result='我帅吗';
 		$con=$obj->Content;
-		$content=$this->doubi($con);
+		$content=$this->qingyunke($con);
 		$result=$this->receive($obj,trim($content));
 		return $result;
 	}
 
-	public function receive($obj,$content)
-	{
+	public function receive($obj,$content){
 		$text="<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><MsgId>%s</MsgId></xml>";
 		return sprintf($text,$obj->FromUserName,$obj->ToUserName,date('Y-m-d H:i:s',time()),$content,$obj->MsgId);
 	}
 
-    public function doubi($msg)
-    {
-		$url="http://www.xiaodoubi.com/bot/chat.php";
-		$curlPost=array('chat'=>$msg);
-
+	public function qingyunke($msg){
+		$url ='http://api.qingyunke.com/api.php?key=free&appid=0&msg='.$msg;
  		$ch = curl_init(); 
-		curl_setopt ($ch, CURLOPT_URL, $url);
-   		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
-		curl_setopt($ch, CURLOPT_TIMEOUT,10);
-    	curl_setopt ($ch, CURLOPT_POST, 1); 
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
-    	$file_contents = curl_exec($ch); 
+		$ch = curl_init($url) ;  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true) ; // 获取数据返回  
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true) ; // 在启用 CURLOPT_RETURNTRANSFER 时候将获取数据返回  
+ 		$file_contents = curl_exec($ch) ;  
 		$curl_errno = curl_errno($ch); 
-
-		if($curl_errno > 0) return '不好意思卧病了';
-    	curl_close($ch); 
-
-		return $file_contents;
+		
+		$a = json_decode($file_contents,true);	
+		return $a['content'];
 	}	
 }
 
 $wc=new chat();
-
 if(!isset($_GET['echostr'])){
 	$wc->logmess();
 	$wc->reponse();
